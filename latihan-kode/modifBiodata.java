f83dfb1ad8d5caf438de68e754cdadbecd7b7062
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2021-03-05 16:26:36 +0700

    Create modifBiodata.java
    -memindahkan rumus segitiga ke dalam class luaskelilingSegi3

---
+++ latihan-kode/modifBiodata.java
@@ -0,0 +1,81 @@
+/**
+ * modifBiodata
+ */
+class Biodata2 {
+    public static void main(String[] args) {
+        //print di java ada 3 macam
+        //println = otomatis enter ke line/baris di bawahnya
+        //print   = mencetak sebaris
+        //printf  = mencetak dengan format tertentu
+        //ingat! setiap akhir print, cantumkan " ; "
+
+        System.out.print("\n");
+        // " \n " digunakan untuk memberi jarak saja 
+        System.out.print(">>>Biodata<<< \n");
+        System.out.print("\n");
+        System.out.println("NIM         :A11.2019.12342");
+        System.out.println("Nama        :Naufal Haidar Rauf");
+        System.out.println("Alamat      :Zebra Mukti Utara II, Pedurungan Kidul");
+        System.out.println("Kota        :Semarang");
+        System.out.println("Kode Pos    :50192");
+        System.out.println("Telp        :-");
+        System.out.println("Hp          :085156967023");
+        System.out.println("Email       :111201912342@mhs.dinus.ac.id");
+        
+        // pemisah saja, biar tidak bingung//
+
+        System.out.print("\n");
+        System.out.print("====> Luas dan Keliling Segitiga <====");
+        System.out.print("\n");
+        int alas,tinggi,luas,sisi,kel;
+        alas=6;
+        tinggi=10;
+        luas=alas*tinggi/2;
+        sisi=4;
+        kel=3*sisi;
+
+        // %d berfungsi untuk memberi jarak dari kiri.
+        // untuk angka
+        
+        System.out.printf("Alas = %d\n ", alas);
+        System.out.printf("Tinggi = %d\n ", tinggi);
+        System.out.printf("Sisi = %d\n ", alas);
+        System.out.print("\n");
+        System.out.printf("Jadi, luas segitiga = %d\n", luas);
+        System.out.printf("Jadi, keliling segitiga = %d\n", kel);
+    }
+
+    
+}
+
+
+/**
+ * memindahkan rumus luas segitiga
+ * dari class biodata
+ */
+ class luaskelilingSegi3 {
+     public static void main(String[] args) {
+        System.out.print("\n");
+        System.out.print("====> Luas dan Keliling Segitiga <====");
+        System.out.print("\n");
+        int alas,tinggi,luas,sisi,kel;
+        alas=6;
+        tinggi=10;
+        luas=alas*tinggi/2;
+        sisi=4;
+        kel=3*sisi;
+
+        // %d berfungsi untuk memberi jarak dari kiri.
+        // untuk angka
+        
+        System.out.printf("Alas = %d\n ", alas);
+        System.out.printf("Tinggi = %d\n ", tinggi);
+        System.out.printf("Sisi = %d\n ", alas);
+        System.out.print("\n");
+        System.out.printf("Jadi, luas segitiga = %d\n", luas);
+        System.out.printf("Jadi, keliling segitiga = %d\n", kel);
+     }
+
+    
+}
+ 
