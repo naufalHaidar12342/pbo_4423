c46c71f65b9ef028dc8329034e8a1320c865856a
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2021-03-06 09:28:05 +0700

    modifikasi dari file tugas

---
+++ tugas1-modif.java
@@ -0,0 +1,30 @@
+class tugas1modif {
+    public static void main(String[] args) {
+        
+        //untuk operasi pertama
+        int angka=-5,angka2=8,angka3=6;
+        int jumlah1;
+        jumlah1=angka+angka2*angka3;
+        System.out.printf("a= %d \n",jumlah1);
+
+        
+
+        //untuk operasi kedua
+        // int b1=55, b2=9;
+        // int jumlah2;
+        // jumlah2=(b1+b2)%b2;
+        // System.out.printf("b= %d \n",jumlah2);
+
+        //untuk operasi ketiga
+        // int c1=20, c2=-3, c3=5;
+        // int jumlah3;
+        // jumlah3=c1+c2*c3/angka2;
+        // System.out.printf("c= %d \n", jumlah3);
+
+        //untuk operasi keempat
+        // int d1=15, d2=3, d3=2;
+        // int jumlah4;
+        // jumlah4=c3+d1/d2*d3-angka2%d2;
+        // System.out.printf("d= %d \n", jumlah4);
+    }
+}
