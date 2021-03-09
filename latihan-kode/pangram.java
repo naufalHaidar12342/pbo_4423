20af5ae1bcd581cbe77cab3b29ac360b69b4ef47
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2021-03-09 12:51:07 +0700

    menambahkan pangram

---
+++ latihan-kode/pangram.java
@@ -0,0 +1,11 @@
+/**
+ * pangram
+ */
+public class pangram {
+    public static void main(String[] args) {
+        System.out.println("the quick brown fox");
+        System.out.println("jumps over lazy dog");
+
+    }
+
+}
