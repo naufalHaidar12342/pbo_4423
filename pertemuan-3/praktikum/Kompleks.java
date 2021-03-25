938c23c88bd08278897f8fcfc42150a97e7c76f4
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2021-03-25 23:22:05 +0700

    memindahkan method untuk menambahkan dua bilangan sebelum method main dijalankan

--- pertemuan-3/praktikum/Kompleks.java
+++ pertemuan-3/praktikum/Kompleks.java
@@ -6,6 +6,15 @@ public class Kompleks {
         this.imag = imag;
     }
 
+    public static Kompleks add(Kompleks bil_1, Kompleks bil_2) {
+        Kompleks temp = new Kompleks(0.0, 0.0);
+
+        temp.real = bil_1.real + bil_2.real;
+        temp.imag = bil_1.imag + bil_2.imag;
+
+        return temp;
+    }
+
     public static void main(String[] args) {
         Kompleks bil_1 = new Kompleks(2.3, 4.5);
         Kompleks bil_2 = new Kompleks(3.4, 5.0);
@@ -16,12 +25,4 @@ public class Kompleks {
         System.out.printf("Jumlah = %.1f +%.1fi", temp.real, temp.imag);
     }
 
-    public static Kompleks add(Kompleks bil_1, Kompleks bil_2) {
-        Kompleks temp = new Kompleks(0.0, 0.0);
-
-        temp.real = bil_1.real + bil_2.real;
-        temp.imag = bil_1.imag + bil_2.imag;
-
-        return temp;
-    }
 }
