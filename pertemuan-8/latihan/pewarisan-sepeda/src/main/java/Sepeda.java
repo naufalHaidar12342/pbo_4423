bc6fe563b43797bcf9f9d4d191f4f059f1d9ad0d
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2021-05-04 09:22:19 +0700

    menambahkan komentar pada program

--- pertemuan-8/latihan/pewarisan-sepeda/src/main/java/Sepeda.java
+++ pertemuan-8/latihan/pewarisan-sepeda/src/main/java/Sepeda.java
@@ -1,4 +1,8 @@
 public class Sepeda {
+    /*membuat variabel bernama 'tipeKendaraan'
+    * yang datanya bertipe String. kita berikan
+    * izin akses 'protected' . artinya, hanya bisa diakses
+    * oleh subclass atau anak dari class 'Sepeda'*/
     protected String tipeKendaraan="kendaraan jalur lambat";
 }
 
