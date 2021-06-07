19fe92329a0d731f8d53901f6e97a874bb05ea48
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2021-06-07 19:11:06 +0700

    mencoba form biodata sedrhana. belum selesai

---
+++ pertemuan-12/latihan-swing/src/formSederhana.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class formSederhana {
+    private JPanel mainWindow;
+    private JLabel windowName;
+    private JLabel namaMhs;
+    private JLabel prodiMhs;
+    private JLabel nimMahasiswa;
+    private JTextField fieldNama;
+    private JComboBox pilihanProdi;
+    private JTextField fieldNIM;
+}
