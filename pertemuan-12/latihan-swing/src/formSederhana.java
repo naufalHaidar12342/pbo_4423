f17df1593c4550dc8add46c42a7fa544b087e479
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2021-06-08 07:32:12 +0700

    menambahkan method untuk mereset semua field

--- pertemuan-12/latihan-swing/src/formSederhana.java
+++ pertemuan-12/latihan-swing/src/formSederhana.java
@@ -1,4 +1,8 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import javax.swing.UIManager;
 
 public class formSederhana {
     private JPanel mainWindow;
@@ -9,4 +13,33 @@ public class formSederhana {
     private JTextField fieldNama;
     private JComboBox pilihanProdi;
     private JTextField fieldNIM;
+    private JButton resetButton;
+    private JButton kirimButton;
+
+    public formSederhana() {
+        //ketika tombol "Reset " ditekan,
+        //akan menghapus isi dari semua field yang ada
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                fieldNama.setText(null);
+                fieldNIM.setText(null);
+                pilihanProdi.setSelectedIndex(0);
+
+            }
+        });
+    }
+
+    /*method main akan menjalankan window/frame yang kita punya
+    * bagian ini di generate secara otamatis
+    * langkah-langkahnya:
+    * 1) klik Alt + Insert
+    * 2) pilih form main()*/
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("formSederhana");
+        frame.setContentPane(new formSederhana().mainWindow);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
